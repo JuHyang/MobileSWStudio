762ded34b8a8ab171a660bc316a96a2c76cd6c2d
JuHyang <kkss2889@naver.com>
2018-11-19 12:07:41 +0900

    Init Backend Project

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+var express = require ("express")
+var bodyParser = require('body-parser')
+var puppeteer = require ("puppeteer")
+var mysql = require('mysql')
+
+var userId = "2015125061"
+var password = ""
+
+
+var conn = mysql.createConnection({
+  host      : 'localhost',
+  user      : 'root',
+  password  : 'dlwngid1!',
+  database  : 'mobileSW'
+})
+
+conn.connect()
+
+var app = express()
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+var url_lms = "https://www.kau.ac.kr/page/login.jsp?ppage=&target_page=act_Portal_Check.jsp@chk1-1"
